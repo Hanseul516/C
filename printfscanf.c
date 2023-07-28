d64f94b0ea4a103284e39141c975bfd70690472f
최한슬 <hanseulo@naver.com>
2023-07-28 13:04:35 +0900

    C 23-07-28

--- printfscanf.c
+++ printfscanf.c
@@ -34,11 +34,17 @@ int main(void)
     // printf("입력값 : %d\n", input);
     // return 0;
 
-    int one, two, three;
-    printf("3개의 정수를 입력하세요: ");
-    scanf("%d,%d,%d", &one, &two, &three);
-    printf("첫번째 값: %d\n", one);
-    printf("두번째 값: %d\n", two);
-    printf("세번째 값: %d\n", three);
-    return 0;
+    // int one, two, three;
+    // printf("3개의 정수를 입력하세요: ");
+    // scanf("%d,%d,%d", &one, &two, &three);
+    // printf("첫번째 값: %d\n", one);
+    // printf("두번째 값: %d\n", two);
+    // printf("세번째 값: %d\n", three);
+    // return 0;
+
+    // 문자(한 글자), 문자열(한 글자 이상)
+    // char str[256]; //256바이트
+    // scanf("%s", str, sizeof(str));
+    // printf("%s\n", str);
+    // return 0;
 }
